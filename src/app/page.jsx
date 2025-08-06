989269f12a44afab87847fc5f5d9cb695eca655e
elvxk <pradiptaramadan9c@gmail.com>
2025-08-07 04:04:02 +0700

    fix(margin): fixing margin between content and footer

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -134,7 +134,7 @@ export default function Home() {
           </div>
         </>
       )}
-      <div className="h-20"></div>
+      <div className="h-28"></div>
       <footer className="absolute bottom-0 self-center mb-4">
         <div className="text-center items-center justify-center flex flex-col">
           <Link
